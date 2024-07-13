e6680efe84a7ef3ba73c655de4b1125a029ab8bc
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2024-07-13 22:28:46 +0530

    created superuser

--- home/views.py
+++ home/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from home.models import Task
 
 # Create your views here.
 def home(request):
+    try:
+        if request.method=="POST":
+            title = request.POST.get('title', '') 
+            desc = request.POST.get('desc', '') 
+            print(title,desc)
+            new_task = Task(title=title,desc=desc)
+            new_task.save()
+    except:
+        working=True
     return render(request,'home.html')
 
 def tasks(request):
-    return render(request,'tasks.html')
+    tasks = Task.objects.all()  
+    return render(request, 'tasks.html', {'tasks': tasks})
+    
